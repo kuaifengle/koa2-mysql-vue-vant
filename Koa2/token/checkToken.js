8fadb9e273e2afc573d0150789034ac5d3244b17
kuaifengle <597286252@qq.com>
2018-08-09 17:17:59 +0800

    增加登出

--- Koa2/token/checkToken.js
+++ Koa2/token/checkToken.js
@@ -40,8 +40,8 @@ const jwt = require('jsonwebtoken');
 //     //看到这里请继续读完下面的代码
 // };
 
-// 上面的 jwt.decode(token, 'sinner77') 只是把信息解密出来，然后再验证是否还在有效期以内
-// 但是这个sinner77的参数是无效的，直接使用jwt.decode(token)或者jwt.decode(token, 'xxxx')
+// 上面的 jwt.decode(token, 'kuaifengle') 只是把信息解密出来，然后再验证是否还在有效期以内
+// 但是这个kuaifengle的参数是无效的，直接使用jwt.decode(token)或者jwt.decode(token, 'xxxx')
 // 解密出来的信息都是一致的，相当于没有对这个token进行是否合法的验证，达不到登录基本的安全性
 // 请使用下面的代码
 module.exports = async ( ctx, next ) => {
